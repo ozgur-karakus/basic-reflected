b7e24dcb7c57906d6374b8b94f1c11d80ec02b52
Özgür Karakuş <103831584+ozgur-karakus@users.noreply.github.com>
2024-09-06 00:10:41 +0300

    Update db.php

--- basic-reflected/db.php
+++ basic-reflected/db.php
@@ -6,7 +6,6 @@ $password = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    // Hata modunu ayarla
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Veritabanı bağlantısı başarılı!";
 } catch (PDOException $e) {
